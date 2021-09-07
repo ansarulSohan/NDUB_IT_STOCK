adbd5e2f2f95bbb40b6acd67aa71bfeaaa26a221
ansarulSohan <ansarulsohan@gmail.com>
2021-09-07 12:08:32 +0600

    Modified Initiation commands

--- initiate.sh
+++ initiate.sh
@@ -1,7 +1,17 @@
+npm i
+echo node installed
+
 if [ -f .env ]
 then
   echo 'env file already exists'
 else
   touch .env
+  echo NODE_ENV = development >> .env
+  echo DEBUG = app:* >> .env
+  echo it_jwtsecret = randomString >> .env
   echo 'env file created.'
 fi
+
+
+
+git pull origin master
